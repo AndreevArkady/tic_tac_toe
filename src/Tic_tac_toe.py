ec752269af53e6b600d4a3b20c4d35b959657d92
AndreevArkady <AndreevArkady@github.com>
2020-10-11 13:17:56 +0300

    Создал класс tic_tac, в нем написал методы __self__, __setitem__, __repr__, __str__ также метод print_field выводящий текущее игровое поле Написал метод make_turn

--- src/Tic_tac_toe.py
+++ src/Tic_tac_toe.py
@@ -1,16 +1,15 @@
-def start_new_game(name):
-    game = tic_tac(name)
-
-
 class tic_tac:
     def __init__(self, player_1, player_2):
         self.player_1 = str(player_1)
         self.player_2 = str(player_2)
-        self.f = [[] * 3] * 3
+        self.f = []
+        self.f.append([])
+        self.f.append([])
+        self.f.append([])
 
-    def __setitem__(self, key1, value):
+    '''def __setitem__(self, key1, value):
         ans = value if isinstance(value, int) else value
-        self.f[key1] = ans
+        self.f[key1] = ans'''
 
     def __repr__(self):
         if isinstance(self, tic_tac):
@@ -20,15 +19,17 @@ class tic_tac:
         return str(s)
 
     def __str__(self):
-        return self.f
+        return str(self.f)
 
     def print_field(self):
         for i in self.f:
             for j in i:
                 print(j, end=' ')
+            print()
+
+    def make_turn(self, raw, column, value):
+        self.f[raw][column] = value
 
 
 if __name__ == '__main__':
-    a = tic_tac('asdad')
-
-
+    a = tic_tac('Arkady', 'Vlad')
