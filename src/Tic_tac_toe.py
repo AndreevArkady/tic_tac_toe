68b5c7b4a2b89dcb7ad51b399a5c978cacb613e3
AndreevArkady <AndreevArkady@github.com>
2020-10-11 17:51:17 +0300

    Создал класс tic_tac, в нем написал методы __self__, __repr__, __str__ также метод print_field выводящий текущее игровое поле Написал метод make_turn Добавил в метод __init__ инициализацию имени первого игрока, также добавил аттрибут index_of_active_player - отоборажающий игрока, чей ход следующий Подкорректировал make_turn, чтобы в случае неверного формата ввода, выводилась информация об этом Добавил метод whose_turn_now - выводящий имя того игрока, чей сейчас ход Добавил функии get_first_player_name и get_first_player_symbol Добавил аттрибут print_field_after_turns отвечающий за вывод игрового поля поле каждого хода (по умолчанию вывод отключен)

--- src/Tic_tac_toe.py
+++ src/Tic_tac_toe.py
@@ -1,15 +1,14 @@
 class tic_tac:
-    turn_symbols = ['x', 'o']
 
-    def __init__(self, player_1, player_2):
-        self.player_names = [player_1, player_2]
-        self.index_of_active_player = 0
-        self.get_first_player_name()
+    def __init__(self, player_0, player_1):
+        self.player_names = [player_0, player_1]
+        self.index_of_active_player = self.get_first_player_name()
         self.players_symbols_dict = self.get_first_player_symbol()
         self.f = []
         self.f.append(['_'] * 3)
         self.f.append(['_'] * 3)  # а по-другому у меня не получилось(
         self.f.append(['_'] * 3)
+        self.print_field_after_turns = 0
 
     def __repr__(self):
         if isinstance(self, tic_tac):
@@ -21,10 +20,13 @@ class tic_tac:
     def __str__(self):
         return str(self.f)
 
+    def switch_field_printing(self):
+        self.print_field_after_turns = 1 - self.print_field_after_turns
+
     def get_first_player_symbol(self):  # заполняем словарь "номер игрока" -> "его символ x или o"
         symbol = input('What is "%s" symbol? x or o -' % str((self.player_names[self.index_of_active_player])))
         if symbol == 'x':
-            return {self.index_of_active_player: 'x', 1 - self.index_of_active_player: 'o'}
+            return {self.index_of_active_player: 'x', 1 - self.index_of_active_player: 'o'}  # indexes are 0 or 1
         else:
             return {self.index_of_active_player: 'o', 1 - self.index_of_active_player: 'x'}
 
@@ -32,22 +34,24 @@ class tic_tac:
         player_name = input('Who start the game? -')
         while not self.player_names.count(player_name):
             player_name = input("Please, enter first player name correctly. -")
-        if player_name == self.player_names[1]:
-            self.index_of_active_player = 1
+        return player_name == self.player_names[1]
 
     def print_field(self):
-        for i in self.f:
-            for j in i:
-                print(j, end=' ')
+        for row in self.f:
+            for item_in_row in row:
+                print(item_in_row, end=' ')
             print()
 
     def make_turn(self, *args):
         try:
             if (1 <= args[0] <= 3) and (1 <= args[1] <= 3):
                 column, row, value = args
-                self.f[3 - row][column - 1] = value  # такая странная формула чтобы синхронизировать формат
-                self.index_of_active_player = 1 - self.index_of_active_player  # ввода и формат хранения таблицы
-                self.print_field()
+                self.f[3 - row][
+                    column - 1] = value  # такая странная формула чтобы синхронизировать формат ввода и формат
+                # хранения таблицы
+                self.index_of_active_player = 1 - self.index_of_active_player
+                if self.print_field_after_turns:
+                    self.print_field()
             else:
                 print("Wrong turn format, enter (column, row, <symbol>)")
         except ValueError:
